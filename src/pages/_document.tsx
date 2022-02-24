8708124cf9bd05f2339308fbc493c4637a023fdb
Krish Gupta <krishguptadev@outlook.com>
2022-02-24 15:51:16 +0530

    chore: directory structure

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Navbar from 'components/Global/Navbar'
+import Navbar from 'components/Layout/Navbar'
 
 export default function Document() {
   return (
