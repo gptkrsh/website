118a366f27d9471e3fe775255e6903030b1b351a
Krish Gupta <krishguptadev@outlook.com>
2022-05-03 11:39:15 +0530

    feat: analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document () {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
         <meta
           name="description"
@@ -28,6 +29,13 @@ export default function Document () {
         <meta name="og:image" content="" />
         <link rel="preconnect" href="https://vitals.vercel-insights.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <Script
+          data-no-cookie
+          strategy='worker'
+          type="text/partytown"
+          async
+          src="https://cdn.splitbee.io/sb.js"
+        ></Script>
       </Head>
       <body className="min-h-screen text-gray-400 bg-gray-900 min-w-[350px] overflow-hidden">
         <Main />
