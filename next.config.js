ae799798f198abcc2d44ccccee4b34eddd508e52
Krish Gupta <krshkun@proton.me>
2022-12-14 17:30:59 +0530

    fix: preact not working

--- next.config.js
+++ next.config.js
@@ -8,23 +8,6 @@ const withMDX = require('@next/mdx')({
 module.exports = withPlugins([withMDX], ({
   eslint: { ignoreDuringBuilds: true },
   typescript: { tsconfigPath: './tsconfig.json' },
-  /**
-   *
-   * @param {import('webpack').Configuration} config
-   * @param {{ buildId: string, dev: Boolean, isServer: Boolean, defaultLoaders: Object }} options
-   * @returns {import('webpack').Configuration}
-   */
-  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
-    if (!dev && !isServer && config.resolve?.alias) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      })
-    }
-
-    return config
-  },
   trailingSlash: false,
   env: {},
   distDir: 'dist',
